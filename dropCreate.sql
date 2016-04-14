704f2cad10b6d4332ca5883ed5708e2016b1bd56
AK <ak15308@my.bristol.ac.uk>
2016-04-14 19:28:21 +0100

    hopefully im on the right track

--- dropCreate.sql
+++ dropCreate.sql
@@ -11,7 +11,17 @@ INSERT INTO Person (name, username, stuId) VALUES ('Alex', 'ak15308', '123450');
 INSERT INTO Person (name, username, stuId) VALUES ('Joseph', 'jl15351', '123451');
 INSERT INTO Person (name, username, stuId) VALUES ('Tom', 'tb15269', '123452');
 
+CREATE TABLE Forum (
+   id INTEGER PRIMARY KEY,
+   name VARCHAR(250) UNIQUE
+);
+
+INSERT INTO Forum (name) VALUES ('Test1');
+INSERT INTO Forum (name) VALUES ('Test2');
+INSERT INTO Forum (name) VALUES ('Test3');
+
 .header on
 .mode column
 
 SELECT * FROM Person LIMIT 5;
+SELECT * FROM Forum LIMIT 5;
