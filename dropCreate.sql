faa79da277cf0442d7e5c589d8613600325cd5ad
AK <ak15308@my.bristol.ac.uk>
2016-04-14 14:50:17 +0100

    added Person values

--- dropCreate.sql
+++ dropCreate.sql
@@ -1,8 +1,17 @@
 DROP TABLE IF EXISTS Person;
 
 CREATE TABLE Person (
-id INTEGER PRIMARYKEY,
+id INTEGER PRIMARY KEY,
 name VARCHAR(100) NOT NULL,
 username VARCHAR(10) NOT NULL UNIQUE,
 stuId VARCHAR(10) NULL
 );
+
+INSERT INTO Person (name, username, stuId) VALUES ('Alex', 'ak15308', '123450');
+INSERT INTO Person (name, username, stuId) VALUES ('Joseph', 'jl15351', '123451');
+INSERT INTO Person (name, username, stuId) VALUES ('Tom', 'tb15269', '123452');
+
+.header on
+.mode column
+
+SELECT * FROM Person LIMIT 5;
